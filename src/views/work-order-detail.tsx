9587b44a92e8c3c03309330e4585b6fd3f65e5af
calgares <carlosalbertoalgares@gmail.com>
2026-01-31 11:59:49 -0600

    Update work-order-detail.tsx

--- src/views/work-order-detail.tsx
+++ src/views/work-order-detail.tsx
@@ -140,6 +140,13 @@ export function WorkOrderDetailView() {
                             </Button>
                         </>
                     )}
+
+                    {ot.estado === 'PAUSADA' && (
+                        <Button onClick={() => handleStatusChange('EN_PROCESO')}>
+                            <Play className="h-4 w-4 mr-2" />
+                            Reanudar Trabajo
+                        </Button>
+                    )}
                 </div>
             </div>
 
